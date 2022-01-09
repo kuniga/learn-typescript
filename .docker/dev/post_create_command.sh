a67baf99b92774640e58388cc653f63b03ccdc0b
Kuniga Yoshinori <kuniga@gmail.com>
2022-01-09 14:03:37 +0900

    chore: Small improvements

--- .docker/dev/post_create_command.sh
+++ .docker/dev/post_create_command.sh
@@ -2,5 +2,14 @@
 
 set -e -x
 
+sudo chown $(id -u -n):$(id -g -n) ./
+
+# copy source code
+if [[ -d ../hostdir/.git && ! -d .git ]]; then
+    rsync -rlpt ../hostdir/.git/ .git/
+    git checkout -- .
+fi
+
+# direnv
 touch .envrc
 direnv allow
